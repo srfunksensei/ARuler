ae91e1253f7697b138952db9bc0d03f6b7fb5b76
srfunksensei <sr.funk.sensei@gmail.com>
2020-10-13 14:19:22 +0200

    initial commit

---
+++ ARuler/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ARuler
+//
+//  Created by MB on 13/10/2020.
+//  Copyright © 2020 MB. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# ARuler
+
+Building an app which can be used to measure the distance between two points in 3d space
+
+## Built with
+
+- [XCode 11.6](https://developer.apple.com/xcode/)
+- [Swift 5](https://developer.apple.com/swift/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
