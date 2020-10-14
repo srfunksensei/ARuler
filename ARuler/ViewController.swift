c8446449b12ab3c35cf313879a6e5130f37c59f5
srfunksensei <sr.funk.sensei@gmail.com>
2020-10-14 11:31:00 +0200

    add dot to AR view on touch

--- ARuler/ViewController.swift
+++ ARuler/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the view's delegate
         sceneView.delegate = self
+        
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -36,5 +38,28 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touchLocation = touches.first?.location(in: sceneView) {
+            let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+            
+            if let hitResult = hitTestResults.first {
+                addDot(at: hitResult)
+            }
+        }
+    }
+    
+    fileprivate func addDot(at hitResult : ARHitTestResult) {
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        
+        let dotGeometry = SCNSphere(radius: 0.005)
+        dotGeometry.materials = [material]
+        
+        let dotNode = SCNNode(geometry: dotGeometry)
+        dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z)
+        
+        sceneView.scene.rootNode.addChildNode(dotNode)
     }
 }
